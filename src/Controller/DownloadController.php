81ab6eda67ff7028af43da4e68c1f53219b3426c
Mehdi-pxl <mehdi.moumite@etu.univ-st-etienne.fr>
2025-12-13 19:44:48 +0100

    portfolio

---
+++ src/Controller/DownloadController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\CvDownloadFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+final class DownloadController extends AbstractController
+{
+    #[Route('/download-cv', name: 'app_download_cv')]
+    public function form(Request $request): Response
+    {
+        $form = $this->createForm(CvDownloadFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            
+            $this->addFlash('success', 'Merci ' . $data['prenom'] . ' ! Votre CV va être téléchargé.');
+            
+            return $this->generatePdf($data);
+        }
+
+        return $this->render('download/form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    private function generatePdf(array $data): Response
+    {
+        $html = $this->renderView('download/pdf.html.twig', [
+            'demandeur' => $data
+        ]);
+
+        $options = new Options();
+        $options->set('defaultFont', 'Arial');
+        $options->set('isRemoteEnabled', true);
+
+        $dompdf = new Dompdf($options);
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+
+        return new Response(
+            $dompdf->output(),
+            Response::HTTP_OK,
+            [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'attachment; filename="CV_Mehdi_MOUMITE.pdf"'
+            ]
+        );
+    }
+}
---
+++ src/Form/CVDownloadFormController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class CvDownloadFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Votre nom'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez saisir votre nom'
+                    ]),
+                    new Assert\Length([
+                        'min' => 2,
+                        'minMessage' => 'Le nom doit contenir au moins {{ limit }} caractères'
+                    ])
+                ]
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Votre prénom'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez saisir votre prénom'
+                    ]),
+                    new Assert\Length([
+                        'min' => 2,
+                        'minMessage' => 'Le prénom doit contenir au moins {{ limit }} caractères'
+                    ])
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'votre.email@example.com'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez saisir votre email'
+                    ]),
+                    new Assert\Email([
+                        'message' => 'L\'email {{ value }} n\'est pas valide'
+                    ])
+                ]
+            ])
+            ->add('entreprise', TextType::class, [
+                'label' => 'Entreprise',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Nom de votre entreprise'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Veuillez saisir le nom de votre entreprise'
+                    ])
+                ]
+            ])
+            ->add('telecharger', SubmitType::class, [
+                'label' => 'Télécharger le CV',
+                'attr' => [
+                    'class' => 'btn btn-primary btn-lg w-100'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
